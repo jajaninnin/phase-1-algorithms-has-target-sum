47c07e356eb2a51bf126af1bba0e50f0e0d6e5c4
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-23 13:23:20 -0400

    alg target sum

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const secondNum = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === secondNum) {
+        return true
+      }
+    }
+  }
+  return false;
 }
 
 /* 
@@ -8,6 +17,7 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+secondNum = target - array[index]
 */
 
 /*
